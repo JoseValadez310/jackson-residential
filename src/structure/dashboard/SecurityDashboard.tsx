932dedb72a1207ead24499ab08ff6a9f4f82fd1b
Jose Valadez <josevaladez310@gmail.com>
2025-01-24 16:24:41 -0800

    added style and chnages to the security page

--- src/structure/dashboard/SecurityDashboard.tsx
+++ src/structure/dashboard/SecurityDashboard.tsx
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom"
 import "../../assets/css/dashboard_css/Security_Dashboard.css"
 //Icons
 import menu_button from "../../assets/images/icons/icons8-arrow.svg"
+import backspace from "../../assets/images/icons/icons8-backspace.svg"
+import clear from "../../assets/images/icons/icons8-x.svg"
 //Componets
 import Clock from "../../components/Clock"
 
@@ -11,9 +13,14 @@ import Clock from "../../components/Clock"
 
 const SecurityDashboard =()=>{
 
+        const green  = "green_ready"
+        const yellow = "yellow_stay"
+        const red  = "red_armed"
+
+        let value 
 
 
-
+console.log(green, yellow, red)
 
     return(
         <div className="security_dashboard">
@@ -26,7 +33,10 @@ const SecurityDashboard =()=>{
                     </Link>
 
                 <div className="nav">
-
+                    <button className="btn_circle"><p>Stay</p></button>
+                    <button className="btn_circle"><p>Away</p></button>
+                    <button className="btn_circle"><p>Disarm</p></button>
+                    <button className="btn_circle"><p>Bypass All</p></button>
                 </div>
                 
 
@@ -40,12 +50,12 @@ const SecurityDashboard =()=>{
             <h1 className="title_info"> Security </h1>
 
 
-            <div className="security_keypad_container">
+            <div className="security_keypad_container" id={green}>
                 
                 <div className="security_keypad_inner_contianer">
 
                     <div className="feedback_viewer"> 
-                            <h1> 123  </h1>
+                            <h1>  {value}  </h1>
                     
                     </div>
                     <div className="keypad">
@@ -61,12 +71,22 @@ const SecurityDashboard =()=>{
                             <button className="btn_circle"><p className="num">8</p></button>
                             <button className="btn_circle"><p className="num">9</p></button>
 
-                            <button className="btn_circle"> <p> Back Space</p></button>
+
+                            <button className="btn_circle" id="btn_active_red"> <img className="btn_image" src={clear} style={{height:"60%"}}/></button>
                             <button className="btn_circle"><p className="num">0</p></button>
-                            <button className="btn_circle"><p>Clear</p></button>
+                            <button className="btn_circle" id="btn_active_red"> <img className="btn_image" src={backspace} style={{height:"68%"}}/></button>
+
+                          
                     </div>
 
                 </div>
+
+            </div>
+
+
+            <div className=" control_container">
+
+               
 
             </div>
 
